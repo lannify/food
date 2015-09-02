bd5e520824d47a983c2f1a674975395826feaabf
lannng <lannguyen309@gmail.com>
2015-09-01 20:34:37 -0700

    fixed conflict in views.php

--- application/controllers/views.php
+++ application/controllers/views.php
@@ -13,7 +13,7 @@ class Views extends CI_Controller {
 	{
 		$this->load->view('mockup');
 	}
-<<<<<<< HEAD
+
 
 	public function landing()
 	{
@@ -29,7 +29,7 @@ class Views extends CI_Controller {
 	{
 		$this->load->view('results');
 	}
-=======
+
 	public function chef()
 	{
 		$this->load->view('chef');
@@ -43,7 +43,7 @@ class Views extends CI_Controller {
 		$this->load->view('cart');
 	}
 
->>>>>>> lan
+
 }
 
 //end of main controller
