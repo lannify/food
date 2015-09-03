4d73cd5183e8733610891d6442057a9b2d0201f6
Lan Nguyen <lannguyen309@gmail.com>
2015-09-03 10:40:31 -0700

    change new_meal.php form

--- application/views/new_meal.php
+++ application/views/new_meal.php
@@ -65,43 +65,7 @@
                                  echo "<div class = 'red text-center'>".$this->session->flashdata('errors')."</div>";
                               }           
 ?>
-
-                  <form action="/meals/create" method="post" role="form" style="display: block;">
-                    <div class="form-group">
-                      <input type="text" name="name" class="form-control" placeholder="Name of Meal" value="">
-                    </div>
-                    <div class="form-group">
-                      <input type="text" name="description" class="form-control" placeholder="Description" value="">
-                    </div>
-                    <div class="form-group">
-                    <label>Select Meal Category</label>
-                      <select name= "category">
-                        <option value="Chinese" class="form-control">Chinese</option>
-                      </select>
-                      <input type="text" name="category" class="form-inline" placeholder="Or add new Category" value="">
-                    </div>
-                    <div class="form-group">
-                      <input type="file" name="meal_images" placeholder="Photos" data-icon="false">
-                    </div>
-                    <div class="form-group">
-                      <input type="text" name="location" class="form-control" placeholder="Location" value="">
-                    </div>
-                    <div class="form-group">
-                      <input type="date" name="date_of_meal" class="form-control" placeholder="Date of Meal" value="">
-                    </div>
-                    <div class="form-group">
-                      <input type="number" name="quantity" class="form-control" placeholder="Serving(s)" value="">
-                    </div>
-                    <div class="form-group">
-                      <input type="text" name="price" class="form-control" placeholder="Price" value="">
-                    </div>
-                    <div class="form-group">
-                      <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3">
-                          <input type="hidden" name="user_id" value="<?= $this->session->userdata('user_id') ?>">
-                          <input type="submit"  class="form-control btn btn-default" value="Create New Meal">
-
-                            <div class="form-group">
+                          <div class="form-group">
                               <label class="col-sm-3 control-label">Meal Name</label>
                               <div class="col-sm-6">
                                 <input type="text" class="form-control" name = "name" placeholder="e.g. Amazing Italian Dinner" required>
