0d676e3a86e411e7aaeb71e6c655f15bb5ad0cd1
lannng <lannguyen309@gmail.com>
2015-09-03 07:14:17 -0700

    minor changes

---
+++ application/views/mockup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mock Up</title>
+</head>
+<body>
+<h2>Food Site Mock Up</h2>
+
+<ul>
+	<li><a href="/views/landing">Landing Page</a></li>
+	<li><a href="">Chef Profile Page</a></li>
+	<li><a href="/views/results">Results Page</a></li>
+	<li><a href="">Customer Dashboard</a></li>
+	<li><a href="">Cart</a></li>
+	<li><a href="">Order/Checkout</a></li>
+	<li><a href="">Meal Page</a></li>
+	<li><a href="">New Chef Registration(Nice to Have)</a></li>
+	<li><a href="">Gallery (Nice to have)</a></li>
+	<li><a href=""></a></li>
+
+
+</ul>
+</body>
+</html>
